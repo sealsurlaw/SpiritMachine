565eff4dd12cc286bdea0a097b35608d189cc8b4
Dylan Wright <sealsurlaw@gmail.com>
2018-11-14 16:22:53 -0800

    Added empty app

---
+++ SpiritMachineTechnician/SpiritMachineTechnician/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpiritMachineTechnician
+//
+//  Created by Dylan Wright on 11/14/18.
+//  Copyright © 2018 spiritmachine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
