964b37cb94d91e7a803932589cc5c4278ddcec2c
jcangeles <jcangeles@outlook.com>
2018-12-13 22:28:13 -0800

    Unit tests fixed: user, machine/alcohol

--- SpiritMachineTech/SpiritMachineTechTests/UserTests.swift
+++ SpiritMachineTech/SpiritMachineTechTests/UserTests.swift
@@ -8,20 +8,25 @@
 
 
 import XCTest
+import ObjectMapper
 @testable import SpiritMachineTech
 
 class UserTests: XCTestCase {
     
     var user: User!
-    var user2: User!
+    var user2: UserJSON!
+    //let userMapper = Mapper<UserJSON>()
     
     override func setUp() {
         super.setUp()
         
-        user = User(id: 1, email: "helo@mail.com", memberSince: Date(timeIntervalSince1970: 1544742926.84))
-        let userJSON = "{“users”:{“user_id”:1,“user_email”:“mail@mail.com”,“user_created”:1544746160.175}}"
-        let json = UserJSON(JSONString: userJSON)
-        user2 = User(id: (json?.id)!, email: (json?.email)!, memberSince: (json?.date)!)
+        user = User(id: 1, email: "mail@mail.com", memberSince: Date(timeIntervalSince1970: 1544742926.84))
+        let userJSON = "{\"user_id\":1,\"user_email\":\"mail@mail.com\",\"user_created\":1544742926.84}"
+        
+//        let json = UserJSON(JSONString: userJSON)
+        user2 = UserJSON(JSONString: userJSON)
+        print(user2)
+        //user2 = User(id: (json?.id!)!, email: (json?.email!)!, memberSince: (json?.date!)!)
         
     }
     
@@ -33,7 +38,7 @@ class UserTests: XCTestCase {
         XCTAssertEqual(user.email, user2.email)
     }
     
-    func test_jsonEamilDate(){
-        XCTAssertEqual(user.memberSince, user2.memberSince)
+    func test_jsonEmailDate(){
+        XCTAssertEqual(user.memberSince, user2.date)
     }
 }
