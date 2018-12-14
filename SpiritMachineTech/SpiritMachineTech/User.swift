0d11d57224390ae4650bcb3d644cd9016fd52242
jcangeles <jcangeles@outlook.com>
2018-12-13 16:26:02 -0800

    account labels

---
+++ SpiritMachineTech/SpiritMachineTech/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  SpiritMachineTech
+//
+//  Created by Juan Angeles on 12/13/18.
+//  Copyright © 2018 spiritmachine. All rights reserved.
+//
+
+import Foundation
+
+
+struct User{
+    let id:Int
+    let email:String
+    let memberSince:Date
+}
