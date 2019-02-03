89f508ba051e08c587ce2058e22b4d4ae0d68443
Dylan Wright <sealsurlaw@gmail.com>
2019-02-02 23:04:57 -0800

    Added python command template under /users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var shell = require('shell-exec');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+
+  shell('python python/script.py')
+    .then(out => {
+      console.log(out.stdout);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  /* GET users listing. */
+
   res.send('respond with a resource');
 });
 
