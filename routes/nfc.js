2b067f63db85663eafcc8dabe3419183b9b04419
Dylan Wright <sealsurlaw@gmail.com>
2019-02-06 04:30:23 +0000

    Added bindings to python NFC library

--- routes/nfc.js
+++ routes/nfc.js
@@ -1,20 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+var shell = require('shell-exec');
 
 /* GET nfc data. */
 router.get('/', function (req, res, next) {
 
     // TODO Call python file to get NFC data
-    var nfcData = 'SomeStringOfDataFromTheNFCTagWhichWillProbablyBePrettyLong'
-    // Get wallet balance from database
-    axios.post('https://spirit-machine.herokuapp.com/wallet/' + nfcData)
-        .then(jsonData => {
-            res.json(jsonData.data);
+    shell('sudo python /home/pi/Programming/MFRC522-python/Read.py')
+    //shell('echo NFC has been disabled')
+        .then(out => {
+
+            var nfcData = out.stdout;
+            // Get wallet balance from database
+            axios.post('https://spirit-machine.herokuapp.com/wallet/' + nfcData)
+                .then(jsonData => {
+                    console.log(jsonData);
+                    res.json(jsonData.data);
+                    //res.json({money: out.stdout})
+                })
+                .catch(err => {
+                    console.log(err);
+                })
         })
         .catch(err => {
             console.log(err);
-        })
+        });
+
 });
 
 module.exports = router;
